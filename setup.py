953e8a8af15c74ae198e80ff13f761c6712f315d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-02-17 23:17:30 +0100

    New version, Py2/3 compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pocket-cli',
-    version='0.1.1',
+    version='0.1.2',
     author='Rakan Alhneiti',
     author_email='rakan.alhneiti@gmail.com',
     url='https://github.com/rakanalh/pocket-api',
@@ -18,6 +18,8 @@ setup(
         'click==6.2',
         'requests==2.9.1',
         'progress==1.2',
+        'future==0.15.2',
+        'six==1.10.0',
         'pocket-api'
     ],
     entry_points={
